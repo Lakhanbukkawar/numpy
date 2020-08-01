118003e9c9c07f4b50e4ffd60ed73a394dec73b1
Lakhanbukkawar <62429133+Lakhanbukkawar@users.noreply.github.com>
2020-08-01 23:48:30 +0530

    Add files via upload

---
+++ matlibport/ex2.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as pt
+x=[1,2,3,6,9]
+y=[5,6,9,8,7]
+
+pt.bar(x,y)
+pt.title("data")
+pt.xlabel("units")
+pt.ylabel("dist")
+pt.show()
---
+++ matlibport/graph.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as py
+
+x=[5,6,8,9,5]
+y=[8,3,2,8,9]
+py.plot(x,y)
+py.show()
